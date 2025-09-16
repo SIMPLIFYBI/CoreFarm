cecd9775015b25543c0efbe5666849160f7d0992
James <james@simplifybi.com>
2025-09-16 13:54:13 +0800

    chore: apply app-wide updates (preview copy, mobile CTA, header/nav/icons and related changes)

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,9 +1,5 @@
-// Re-export the preview marketing page at the new /home route
-export { default } from "../preview/page";
+import PreviewPage from "../preview/page";
 
-return (
-  // removed gradient classes, using transparent bg instead
-  <div className="bg-transparent">
-    {/* ...existing code... */}
-  </div>
-);
+export default function HomePage() {
+  return <PreviewPage />;
+}
